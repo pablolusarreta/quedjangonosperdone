ccdefacf0ce1577ea80f472f80e4ed3f2a9b8fb0
Pablo Lusarreta <pablo.lusarreta@gmail.com>
2023-11-28 16:55:11 +0100

    Obtimizando

--- docs/index.js
+++ docs/index.js
@@ -1,25 +1,53 @@
 const idiomas = ['castellano', 'euskera']
-let idioma_sel, foto, DATOS
+let DATOS, temas, pie
 const inicio = () => {
     // CONSTANTES
-    const temas = document.getElementById('lista')
-    const player = document.getElementById('player')
-    const pie = document.getElementsByTagName('footer')[0]
+    temas = document.getElementById('lista')
+
+    pie = document.getElementsByTagName('footer')[0]
     // ARRANQUE
     fetch("temas.json")
         .then(response => response.json())
         .then(data => {
             DATOS = data
-            for (let i in data.tema) {
-                temas.innerHTML += `<div onclick="play('${data.tema[i]}.mp3')">${data.tema[i]}</div>`
+            for (let i in DATOS.tema) {
+                temas.innerHTML += `<div id="tema${[i]}" onclick="play('${DATOS.tema[i]}.mp3',this)">${DATOS.tema[i]}</div>`
             }
-            temas.innerHTML += `<div id="player"></div>`
-
+            reescala()
+            movil()
         })
 }
-const play = tema => {
+const inicia_lista = () => {
+    for (let i in DATOS.tema) {
+        document.getElementById('tema'+i).style.color = 'var(--color2)'
+    }  
+}
+const play = (tema, ob) => {
+    inicia_lista()
+    let player = document.getElementById('player')
     player.innerHTML = `<audio controls autoplay>
                         <source src="MP3/${tema}" type="audio/mpeg">
                     </audio>`
+    ob.style.color = 'var(--color3)'
 }
+const reescala = () => {
+    let ancho = window.innerWidth;
+    let zoom = ancho / 1500
+    zoom = zoom < 1 ? 1 : zoom
+    console.log(ancho, zoom)
+    document.body.style.zoom = zoom
+}
+const movil = () => {
+    if (navigator.userAgent.match(/Android/i)
+        || navigator.userAgent.match(/webOS/i)
+        || navigator.userAgent.match(/iPhone/i)
+        || navigator.userAgent.match(/iPad/i)
+        || navigator.userAgent.match(/iPod/i)
+        || navigator.userAgent.match(/BlackBerry/i)
+        || navigator.userAgent.match(/Windows Phone/i)) {
+        console.log('Movil')
+        //document.getElementById("estilo").setAttribute('href', 'movil.css')
+    }
+}
+window.onresize = reescala
 window.onload = inicio
