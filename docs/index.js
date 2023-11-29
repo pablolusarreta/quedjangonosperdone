039a6801eafe3ffd1767fed691ea2731233d2499
Pablo Lusarreta <pablo.lusarreta@gmail.com>
2023-11-29 16:48:56 +0100

    Video implementado

--- docs/index.js
+++ docs/index.js
@@ -1,9 +1,9 @@
 const idiomas = ['castellano', 'euskera']
-let DATOS, temas, pie, IF;
-let temSel = 'tema0'
+let DATOS, elementos, pie, IF;
+let temSel = 'elemento0'
 const inicio = () => {
     // CONSTANTES
-    temas = document.getElementById('lista')
+    elementos = document.getElementById('lista')
     pie = document.getElementsByTagName('footer')[0]
     IF = document.getElementById("imgFondo")
     IF.addEventListener('load', () => {
@@ -11,37 +11,42 @@ const inicio = () => {
     })
     IF.setAttribute("src", "img/Portada.jpg")
     // ARRANQUE
-    fetch("temas.json")
+    fetch("datos.json")
         .then(response => response.json())
         .then(data => {
             DATOS = data
-            for (let i in DATOS.tema) {
-                temas.innerHTML += `<div id="tema${[i]}" onclick="play('${DATOS.tema[i]}.mp3',this)" class="tema">
-                ${DATOS.tema[i]}</div>`
+            for (let i in DATOS.elementos) {
+                elementos.innerHTML += `<div id="elemento${[i]}" onclick="play('${DATOS.elementos[i].direccion}.mp3','${DATOS.elementos[i].tipo}',this)" class="elemento">
+                <img src="img/${DATOS.elementos[i].tipo}.png" alt="·">${DATOS.elementos[i].nombre}</div>`
             }
             reescala()
             movil()
         })
 }
-const play = (tema, ob) => {
-    document.getElementById(temSel).style.color = 'var(--color2)'
-    let reproductor = document.getElementById('reproductor')
-    if (reproductor.firstChild) reproductor.removeChild(reproductor.firstChild)
-    const audio = document.createElement("audio")
-    audio.setAttribute("controls", '')
-    audio.setAttribute("autoplay", '')
-    audio.addEventListener('ended', () => {
-        reproductor.removeChild(reproductor.firstChild)
+const play = (url, tipo, ob) => {
+    
+    if (tipo === "mp3") {
         document.getElementById(temSel).style.color = 'var(--color2)'
-    })
-    const source = document.createElement("source")
-    source.setAttribute("src", `MP3/${tema}`)
-    source.setAttribute("type", 'audio/mpeg')
-    audio.appendChild(source)
-    reproductor.appendChild(audio)
-
-    temSel = ob.id
-    ob.style.color = 'var(--color3)'
+        let reproductor = document.getElementById('reproductor')
+        if (reproductor.firstChild) reproductor.removeChild(reproductor.firstChild)
+        const audio = document.createElement("audio")
+        audio.setAttribute("controls", '')
+        audio.setAttribute("autoplay", '')
+        audio.addEventListener('ended', () => {
+            reproductor.removeChild(reproductor.firstChild)
+            document.getElementById(temSel).style.color = 'var(--color2)'
+        })
+        const source = document.createElement("source")
+        source.setAttribute("src", `MP3/${url}`)
+        source.setAttribute("type", 'audio/mpeg')
+        audio.appendChild(source)
+        reproductor.appendChild(audio)
+        temSel = ob.id
+        ob.style.color = 'var(--color3)'
+    }else if(tipo === 'video'){
+        window.open(url, '_blank')
+    }
+    
 }
 const reescala = () => {
     let ancho = window.innerWidth;
@@ -65,7 +70,6 @@ const movil = () => {
         pie.style.width = "100%"
         pie.style.backgroundColor = "#000"
         IF.style.width = "100%"
-
     }
 }
 window.onresize = reescala
