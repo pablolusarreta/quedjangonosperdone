4e17a5dad0f7e55b70951a1d0cea12fab5209d2b
Pablo Lusarreta <pablo.lusarreta@gmail.com>
2023-11-30 09:34:20 +0100

    responsibe 2.0

--- docs/index.js
+++ docs/index.js
@@ -1,8 +1,7 @@
 const idiomas = ['castellano', 'euskera']
 let DATOS, elementos, pie, IF;
 let temSel = 'elemento0'
-const inicio = () => {
-    // CONSTANTES
+const inicio = () => {            
     elementos = document.getElementById('lista')
     pie = document.getElementsByTagName('footer')[0]
     IF = document.getElementById("imgFondo")
@@ -20,11 +19,10 @@ const inicio = () => {
                 <img src="img/${DATOS.elementos[i].tipo}.png" alt="·">${DATOS.elementos[i].nombre}</div>`
             }
             reescala()
-            movil()
+
         })
 }
-const play = (url, tipo, ob) => {
-    
+const play = (url, tipo, ob) => {   
     if (tipo === "mp3") {
         document.getElementById(temSel).style.color = 'var(--color2)'
         let reproductor = document.getElementById('reproductor')
@@ -53,23 +51,6 @@ const reescala = () => {
     let zoom = ancho / 1500
     zoom = zoom < 1 ? 1 : zoom
     document.body.style.zoom = zoom
-}
-const movil = () => {
-    if (navigator.userAgent.match(/Android/i)
-        || navigator.userAgent.match(/webOS/i)
-        || navigator.userAgent.match(/iPhone/i)
-        || navigator.userAgent.match(/iPad/i)
-        || navigator.userAgent.match(/iPod/i)
-        || navigator.userAgent.match(/BlackBerry/i)
-        || navigator.userAgent.match(/Windows Phone/i)) {
-        console.log('Movil')
-        //document.getElementById("estilo").setAttribute('href', 'movil.css')
-        document.body.style.zoom = "2"
-        document.getElementById('titulo').style.fontSize = "40px"
-        pie.style.width = "100%"
-        pie.style.backgroundColor = "#000"
-        IF.style.width = "100%"
-    }
 }
 window.onresize = reescala
 window.onload = inicio
