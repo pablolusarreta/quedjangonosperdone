14d44c88025702ac62360f670dca372047297716
Pablo Lusarreta <pablo.lusarreta@gmail.com>
2025-03-15 21:16:54 +0100

    Mas cambios esteticos

--- docs/index.js
+++ docs/index.js
@@ -27,35 +27,24 @@ const elementos = $('#lista')
 const pie = $('footer')
 
 let temSel = 'elemento0'
-window.addEventListener('load',()=>{
-//
 
 
-if (screen.orientation) {
-    screen.orientation.lock("portrait").then(() => {
-        console.log("Orientación bloqueada en modo retrato.");
-    }).catch((error) => {
-        console.error("Error al bloquear la orientación:", error);
-    });
-} else {
-    console.log("La API de orientación de pantalla no es compatible con este navegador.");
-}
 // ARRANQUE
-
-const esmovil = (/Android|iPhone|iPad|Mobile/i.test(navigator.userAgent))
-const t = new Date().getTime()
-fetch(`datos.json?t=${t}`)
-    .then(response => response.json())
-    .then(data => {
-        data.elementos.reverse()
-        const DATOS = data
-        for (let i in DATOS.elementos) {
-            elementos.innerHTML += `<div id="elemento${[i]}" onclick="play('${DATOS.elementos[i].direccion}.mp3','${DATOS.elementos[i].tipo}',this)" class="elemento">
+window.addEventListener('load', () => {
+    const esmovil = (/Android|iPhone|iPad|Mobile/i.test(navigator.userAgent))
+    const t = new Date().getTime()
+    fetch(`datos.json?t=${t}`)
+        .then(response => response.json())
+        .then(data => {
+            data.elementos.reverse()
+            const DATOS = data
+            for (let i in DATOS.elementos) {
+                elementos.innerHTML += `<div id="elemento${[i]}" onclick="play('${DATOS.elementos[i].direccion}.mp3','${DATOS.elementos[i].tipo}',this)" class="elemento">
                 <img src="img/${DATOS.elementos[i].tipo}.png" alt="·">${DATOS.elementos[i].nombre}</div>`
-        }
-        for (let i in DATOS.pie) {
-            pie.innerHTML += esmovil || DATOS.pie[i].pc ? `<div><a href="${DATOS.pie[i].direccion}" target="_blank">
+            }
+            for (let i in DATOS.pie) {
+                pie.innerHTML += esmovil || DATOS.pie[i].pc ? `<div><a href="${DATOS.pie[i].direccion}" target="_blank">
                 <img src="${DATOS.pie[i].img}"><br>${DATOS.pie[i].nombre}</a></div>` : ''
-        }
-    })    
+            }
+        })
 })
